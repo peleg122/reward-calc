53c382a39627ca8c9f08881bbc0dcce2980420fb
Peleg Zborovski <peleg122@gmail.com>
2024-07-10 12:00:16 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ async function fetchData() {
                 'Access-Control-Allow-Origin': '*'
             }
         });
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
