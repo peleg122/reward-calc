bf352972168c2cd93a16e95ec1e99f421f974f34
Peleg Zborovski <peleg122@gmail.com>
2024-07-10 11:53:55 +0300

    making % collected

--- scripts.js
+++ scripts.js
@@ -1,24 +1,3 @@
-async function fetchData() {
-    const walletAddress = document.getElementById('walletAddress').value;
-    const apiUrl = `https://api.keungz.com/well-claim/${walletAddress}`;
-
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        displayData(data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
 function displayData(data) {
     const gallery = document.getElementById('gallery');
     gallery.innerHTML = '';
@@ -35,17 +14,21 @@ function displayData(data) {
             totalRewards += rewards;
 
             const imgUrl = data.nftImages[classType][item.contractArguments.NFTClaimable.tokenId];
+            const percentage = (Number(rewards) / Number(airdrop) * 100).toFixed(2);
+
             const galleryItem = document.createElement('div');
             galleryItem.className = 'gallery-item';
             galleryItem.innerHTML = `
                 <img src="${imgUrl}" alt="${classType} Image">
                 <p>Airdrop: ${(airdrop / BigInt(1e18)).toString()}</p>
                 <p>Rewards: ${(rewards / BigInt(1e18)).toString()}</p>
+                <p>Percentage: ${percentage}%</p>
             `;
             gallery.appendChild(galleryItem);
         });
     });
 
+    const totalPercentage = (Number(totalRewards) / Number(totalAirdrop) * 100).toFixed(2);
     const totalAmount = document.getElementById('totalAmount');
-    totalAmount.textContent = `Total Airdrop: ${(totalAirdrop / BigInt(1e18)).toString()} | Total Rewards: ${(totalRewards / BigInt(1e18)).toString()}`;
+    totalAmount.textContent = `Total Airdrop: ${(totalAirdrop / BigInt(1e18)).toString()} | Total Rewards: ${(totalRewards / BigInt(1e18)).toString()} | Total Percentage: ${totalPercentage}%`;
 }
